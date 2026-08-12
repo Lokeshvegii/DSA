a023e78d44da8d36dfc423327facc8d419234e89
Lokesh Vegi <lokeshhvegiii@gmail.com>
2026-08-12 23:47:13 +0530

    Time: 67 ms (59.25%) | Memory: 89 MB (56.07%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -4,14 +4,18 @@ class Solution {
         int n = nums.length;
         int count = 0;
         int i = 0;
+
         for(int j = 0; j < n; j++) {
-            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+             mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+
             while(mp.get(nums[j]) > k) {
                     mp.put(nums[i], mp.get(nums[i]) - 1);
                     i++;
                 }
+
             count = Math.max(count, j - i + 1);
         }
+        
         return count;
     }
 }
